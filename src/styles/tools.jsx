5e7dd9aee70d9bfd376c1b6695a92dd2548795e8
freddy velarde <freddyvelarde59#gmail.com>
2022-07-25 20:55:58 -0400

    adding theme colors and fonts

---
+++ src/styles/tools.jsx
@@ -0,0 +1,24 @@
+export const darkTheme = {
+  primary: "#09172D",
+  secondary: "#172A46",
+  third: "#64FFDB",
+  button: "#3E1D98",
+  text: "#C8D2F2",
+  gray: "#9EA9C8",
+  hover: "#323697",
+}
+
+export const lightTheme = {
+  primary: "#ffffff",
+  secondary: "#FAFAFA",
+  third: "#AC739B",
+  button: "#5D59AA",
+  text: "#242423",
+  gray: "#858585",
+  hover: "#74ABD6",
+}
+
+export const fonts = {
+  titles: "'Noto Serif', serif",
+  text: "'Oswald', sans-serif"
+}
